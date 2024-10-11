ff767826fa38d7da4c270d87055a0dd9328f8320
Júlia Soares <juliasoares.contato@gmail.com>
2024-10-10 22:02:34 -0300

    TP - Connect DB

---
+++ TP/Connect/src/DAO/Clientes.java
@@ -0,0 +1,147 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+public class Clientes {
+    private int idCli; // NUMERIC(9,0)
+    private String nomeCli; // VARCHAR(50)
+    private String endeCli; // VARCHAR(50)
+    private String numeCli; // VARCHAR(8)
+    private String complCli; // VARCHAR(20)
+    private String bairCli; // VARCHAR(20)
+    private String cidaCli; // VARCHAR(30)
+    private String ufCli; // CHAR(2)
+    private String cepCli; // CHAR(8)
+    private String foneCli; // VARCHAR(13)
+    private String cpfCli; // CHAR(11)
+    private String dataNasc; // DATETIME
+    private String cnpjCli; // CHAR(14)
+
+    // Construtor vazio
+    public Clientes() {}
+
+    public String dadosSQLInsert(){
+    String dadosClientes;
+    dadosClientes = "'"
+            + this.getIdCli() + "','"
+            + this.getNomeCli() + "','"
+            + this.getEndeCli() + "','"
+            + this.getNumeCli() + "','"
+            + this.getComplCli() + "','"
+            + this.getBairCli() + "','"
+            + this.getCidaCli() + "','"
+            + this.getUfCli() + "','"
+            + this.getCepCli() + "','"
+            + this.getFoneCli() + "','"
+            + this.getCpfCli() + "','"
+            + this.getDataNasc() + "','"
+            + this.getCnpjCli() + "'";
+    return dadosClientes;
+    }
+    // Getters e Setters
+    public int getIdCli() {
+        return idCli;
+    }
+
+    public void setIdCli(int idCli) {
+        this.idCli = idCli;
+    }
+
+    public String getNomeCli() {
+        return nomeCli;
+    }
+
+    public void setNomeCli(String nomeCli) {
+        this.nomeCli = nomeCli;
+    }
+
+    public String getEndeCli() {
+        return endeCli;
+    }
+
+    public void setEndeCli(String endeCli) {
+        this.endeCli = endeCli;
+    }
+
+    public String getNumeCli() {
+        return numeCli;
+    }
+
+    public void setNumeCli(String numeCli) {
+        this.numeCli = numeCli;
+    }
+
+    public String getComplCli() {
+        return complCli;
+    }
+
+    public void setComplCli(String complCli) {
+        this.complCli = complCli;
+    }
+
+    public String getBairCli() {
+        return bairCli;
+    }
+
+    public void setBairCli(String bairCli) {
+        this.bairCli = bairCli;
+    }
+
+    public String getCidaCli() {
+        return cidaCli;
+    }
+
+    public void setCidaCli(String cidaCli) {
+        this.cidaCli = cidaCli;
+    }
+
+    public String getUfCli() {
+        return ufCli;
+    }
+
+    public void setUfCli(String ufCli) {
+        this.ufCli = ufCli;
+    }
+
+    public String getCepCli() {
+        return cepCli;
+    }
+
+    public void setCepCli(String cepCli) {
+        this.cepCli = cepCli;
+    }
+
+    public String getFoneCli() {
+        return foneCli;
+    }
+
+    public void setFoneCli(String foneCli) {
+        this.foneCli = foneCli;
+    }
+
+    public String getCpfCli() {
+        return cpfCli;
+    }
+
+    public void setCpfCli(String cpfCli) {
+        this.cpfCli = cpfCli;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getCnpjCli() {
+        return cnpjCli;
+    }
+
+    public void setCnpjCli(String cnpjCli) {
+        this.cnpjCli = cnpjCli;
+    }
+}
---
+++ TP/Connect/src/DAO/ConnectDAO.java
@@ -0,0 +1,83 @@
+package DAO;
+ 
+ 
+import java.util.List;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+// import javax.swing.table.AbstractTableModel;
+ 
+/**
+*
+* @author Prof. Corredato - Controle de conexão com o banco de dados
+*/
+public class ConnectDAO {
+    Connection con;
+    public Connection connectDB(){
+        // JOptionPane.showMessageDialog(null, "Inicia a classe para conexão com SQL SERVER!");
+ 
+        String caminho = "jdbc:sqlserver://localhost:1433;databaseName=MOV_CONTA_CORRENTE;"  // Troque VIDEOLOCADORA para MOV_CONTA_CORRENTE
+                + "encrypt=true;trustServerCertificate=true;"; 
+        String usuario = "sa";
+        String senha = ".";
+        try {
+            con = DriverManager.getConnection(caminho, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+            JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+            JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+        }
+        return con;
+        // con.close();
+    }
+    /*
+    * Rotina para inserir um registro novo no banco de dados
+    */
+    //public void insereRegistroJFBD(int idCliente, String nome, String endereco, String bairro, String cidade, String estado, String telefone, String email, String sexo){
+    // public void insereRegistroJFBD(String banco, Cliente novo_cliente){
+    public void insereRegistroJFBD(String tabela, String strDados){
+        String caminho = "jdbc:sqlserver://localhost:1433;databaseName=MOV_CONTA_CORRENTE;encrypt=true;trustServerCertificate=true;"; // O "control" representa a minha database 
+        String usuario = "sa";
+        String senha = ".";
+        {
+ 
+            try {
+                con = DriverManager.getConnection(caminho, usuario, senha);
+                // JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+            }
+            Statement stmt;
+            try {
+                stmt = con.createStatement();
+                // Cria a string com a sentença SQL para inserir registro
+ 
+                     String sql = "INSERT INTO dbo."+tabela+" "
+                            + "VALUES ("+ strDados+")";
+                    JOptionPane.showMessageDialog(null, "String de Insert: "+sql);
+ 
+                 try {
+                // Executar a sentença de insert
+                    stmt.executeUpdate(sql);
+                    JOptionPane.showMessageDialog(null, "Inclusão executada com sucesso!");
+                } catch (SQLException erro) {
+                    JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+                    JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+                    JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+                }
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ConnectDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
